1992cca4e46ad24cdb608ecabb40f1546b6c0a97
Nasakol <nasakol@gmail.com>
2017-12-03 15:33:07 +0700

    add InventoryControllerTest

--- src/test/java/org/springframework/samples/mealOnWheel/web/InventoryControllerTest.java
+++ src/test/java/org/springframework/samples/mealOnWheel/web/InventoryControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.samples.mealOnWheel.model.Event;
 import org.springframework.samples.mealOnWheel.model.Food;
+import org.springframework.samples.mealOnWheel.model.Inventory;
 import org.springframework.samples.mealOnWheel.service.CompanyService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,13 +26,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/mvc-core-config.xml", "classpath:spring/mvc-test-config.xml"})
 @WebAppConfiguration
-public class FoodControllerTest {
+public class InventoryControllerTest {
 
-    private static final int TEST_FOOD_ID = 1;
-    private static final String VIEWS_FOOD_CREATE_OR_UPDATE_FORM = "foods/createOrUpdateFoodForm";
+    private static final int TEST_INVENTORY_ID = 1;
+    private static final String VIEWS_INVENTORY_CREATE_OR_UPDATE_FORM = "inventorys/createOrUpdateInventoryForm";
 
     @Autowired
-    private FoodController foodController;
+    private InventoryController inventoryController;
 
     @Autowired
     private CompanyService companyService;
@@ -42,61 +43,67 @@ public class FoodControllerTest {
 
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(foodController).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(inventoryController).build();
 
-        Food food = new Food();
-        food.setId(TEST_FOOD_ID);
-        given(this.companyService.findFoodById(TEST_FOOD_ID)).willReturn(food);
+        given(this.companyService.findInventoryById(TEST_INVENTORY_ID)).willReturn(new Inventory());
     }
 
     @Test
     public void testInitCreationForm() throws Exception {
-        ResultActions c = mockMvc.perform(get("/foods/new"))
+        ResultActions c = mockMvc.perform(get("/inventorys/new"))
             .andExpect(status().isOk())
-            .andExpect(view().name(VIEWS_FOOD_CREATE_OR_UPDATE_FORM));
+            .andExpect(view().name(VIEWS_INVENTORY_CREATE_OR_UPDATE_FORM));
     }
 
     @Test
-    public void testProcessFoodFormSuccess() throws Exception {
-        mockMvc.perform(post("/foods/new")
+    public void testProcessInventoryForm() throws Exception {
+        mockMvc.perform(post("/inventorys/new")
             .param("name", "nn")
             .param("type", "ty")
-            .param("amount", "4")
-            .param("expireDate", "2017/06/13")
+            .param("amount", "3")
+            .param("purchaseDate", "2017/01/01")
         )
             .andExpect(status().is3xxRedirection())
-            .andExpect(view().name("redirect:/foods"));
+            .andExpect(view().name("redirect:/inventorys"));
     }
 
     @Test
-    public void testProcessFoodFormFailed() throws Exception {
-        mockMvc.perform(post("/foods/new")
-            .param("expireDate", "2017/50/50")      // wrong date
+    public void testProcessInventoryFormFailed() throws Exception {
+        mockMvc.perform(post("/inventorys/new")
+            .param("purchaseDate", "2017/50/50")      // wrong date
         )
             .andExpect(status().isOk())
-            .andExpect(view().name("foods/createOrUpdateFoodForm"));
+            .andExpect(view().name(VIEWS_INVENTORY_CREATE_OR_UPDATE_FORM));
     }
 
 
     @Test
     public void testInitUpdateForm() throws Exception {
-        ResultActions c = mockMvc.perform(get("/foods/{foodId}/edit", 1))
+        ResultActions c = mockMvc.perform(get("/inventorys/{inventoryId}/edit", TEST_INVENTORY_ID))
             .andExpect(status().isOk())
-            .andExpect(view().name(VIEWS_FOOD_CREATE_OR_UPDATE_FORM));
+            .andExpect(view().name(VIEWS_INVENTORY_CREATE_OR_UPDATE_FORM));
     }
 
 
     @Test
     public void testProcessUpdateForm() throws Exception {
-        ResultActions c = mockMvc.perform(post("/foods/{foodId}/edit", 1)
+        ResultActions c = mockMvc.perform(post("/inventorys/{inventoryId}/edit", TEST_INVENTORY_ID)
+            .param("id", "1")
             .param("name", "nn")
             .param("type", "ty")
             .param("amount", "4")
             .param("expireDate", "2017/06/13")
         )
             .andExpect(status().is3xxRedirection())
-            .andExpect(view().name("redirect:/foods"));
+            .andExpect(view().name("redirect:/inventorys"));
     }
 
+
+    @Test
+    public void testShowInventoryList() throws Exception {
+        ResultActions c = mockMvc.perform(get("/inventorys"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("inventorys/inventoryList"));
+    }
 }
 
